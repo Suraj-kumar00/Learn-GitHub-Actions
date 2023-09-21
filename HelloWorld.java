ee00bbbcf52fde2002e8e00ab1e7c60a888413dc
Suraj Kumar <123288511+Suraj-kumar00@users.noreply.github.com>
2023-09-21 18:26:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWrold{
- public static void main(String[] args){
-   System.out.println("Hello Developers, I'm doing scaler open source september challenge");
-}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Developers, I'm doing scaler open source september challenge");
+    }
 }
